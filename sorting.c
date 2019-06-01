fc4a896043be43f4af798f95fba857e2a8104543
HyeminNoh <shrmffkfk@naver.com>
2019-06-02 03:58:44 +0900

    정렬 코드 (주석 인코딩 문제 해결)

--- sorting.c
+++ sorting.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <memory.h>
 #include <ctype.h>
-#include "sorting.h"
+#include "nhm.h"
 
 double start, end;
 
@@ -19,7 +19,7 @@ void merge_title(movieData list[], int left, int mid, int right)
 	i = left; j = mid + 1; k = left;
 
 	while (i <= mid && j <= right) {
-		if (strcmp(list[i].title, list[j].title) > 0)
+		if (strcmp(list[i].title, list[j].title) > 0)		//타이틀만 정렬 돌릴때는 abc순이 맞았는데 타이틀 후 년도를 돌리니까 타이틀이 역순됨. 그래서 <0에서 >0으로 바꾼거임 이거 보고서에 넣어야지
 			sorted[k++] = list[i++];
 		else
 			sorted[k++] = list[j++];
@@ -74,7 +74,7 @@ void merge_sort(movieData list[], int left, int right, char* type)
 	}
 }
 
-
+//삽입
 void insertion_sort(movieData list[], int n, char *type)
 {
 	int unsorted;
@@ -104,7 +104,7 @@ void insertion_sort(movieData list[], int n, char *type)
 		}
 	}
 }
-
+//
 
 int partition(movieData list[], int left, int right, char* type)
 {
@@ -115,54 +115,60 @@ int partition(movieData list[], int left, int right, char* type)
 	low = left;
 	high = right + 1;
 	if (strcmp(type, "title") == 0) {
-		strcpy(pivot, list[left].title); 	
+		strcpy(pivot, list[left].title); 	/* 피벗 설정 */
 		do {
 			do
 				low++;
+			/* 왼쪽 리스트에서 피벗보다 큰 레코드 선택 */
 			while (low <= right && strcmp(list[low].title, pivot) < 0);
 			do
 				high--;
+			/* 오른쪽 리스트에서 피벗보다 작은 레코드 선택 */
 			while (high >= left && strcmp(list[high].title, pivot) > 0);
-			if (low < high) SWAP(list[low], list[high], temp[0]);
-		} while (low < high);
+			if (low < high) SWAP(list[low], list[high], temp[0]); /* 선택된 두 레코드 교환 */
+		} while (low < high);	  /* 인덱스 i,j가 엇갈리지 않는 한 반복 */
 
-		SWAP(list[left], list[high], temp[0]);
+		SWAP(list[left], list[high], temp[0]); /* 인텍스 j가 가리키는 레코드와 피벗 교환 */
 		return high;
 	}
 	else if (strcmp(type, "years") == 0) {
-		strcpy(pivot, list[left].years);
+		strcpy(pivot, list[left].years); 	/* 피벗 설정 */
 		do {
 			do
 				low++;
+			/* 왼쪽 리스트에서 피벗보다 큰 레코드 선택 */
 			while (low <= right && strcmp(list[low].years, pivot) < 0);
 			do
 				high--;
+			/* 오른쪽 리스트에서 피벗보다 작은 레코드 선택 */
 			while (high >= left && strcmp(list[high].years, pivot) > 0);
-			if (low < high) SWAP(list[low], list[high], temp[0]);
-		} while (low < high);
+			if (low < high) SWAP(list[low], list[high], temp[0]); /* 선택된 두 레코드 교환 */
+		} while (low < high);	  /* 인덱스 i,j가 엇갈리지 않는 한 반복 */
 
-		SWAP(list[left], list[high], temp[0]);
+		SWAP(list[left], list[high], temp[0]); /* 인텍스 j가 가리키는 레코드와 피벗 교환 */
 		return high;
 	}
 }
 
+//
 void quick_sort(movieData list[], int left, int right, char* type)
 {
-	if (left < right) {
+	if (left < right) {     /* 리스트에 2개 이상의 레코드가 있을 경우 */
 		if (strcmp(type, "title") == 0) {
 			int q = partition(list, left, right, type);
-			quick_sort(list, left, q - 1, type);
-			quick_sort(list, q + 1, right, type);
+			quick_sort(list, left, q - 1, type);         /* 왼쪽 부분리스트를 퀵정렬 */
+			quick_sort(list, q + 1, right, type);       /* 오른쪽 부분리스트를 퀵정렬 */
 		}
 		else if (strcmp(type, "years") == 0) {
 			int q = partition(list, left, right, type);
-			quick_sort(list, left, q - 1, type);
-			quick_sort(list, q + 1, right, type);
+			quick_sort(list, left, q - 1, type);         /* 왼쪽 부분리스트를 퀵정렬 */
+			quick_sort(list, q + 1, right, type);       /* 오른쪽 부분리스트를 퀵정렬 */
 		}
 	}
 }
+//
 
-
+// 우선 순위 큐인 히프를 이용한 정렬
 void heap_sort(movieData list[], int n, char *type)
 {
 	int i;
@@ -223,12 +229,12 @@ element delete_max_heap(HeapType *h, char *type)
 	child = 2;
 	if (strcmp(type, "title") == 0) {
 		while (child <= h->heap_size) {
-			
+			// 현재 노드의 자식노드중 더 작은 자식노드를 찾는다.
 			if ((child < h->heap_size) &&
 				(strcmp(h->heap[child]->title, h->heap[child + 1]->title) > 0))
 				child++;
 			if (strcmp(temp->title, h->heap[child]->title) <= 0) break;
-			
+			// 한단계 아래로 이동
 			h->heap[parent] = h->heap[child];
 			parent = child;
 			child *= 2;
@@ -236,12 +242,12 @@ element delete_max_heap(HeapType *h, char *type)
 	}
 	else if (strcmp(type, "years")==0) {
 		while (child <= h->heap_size) {
-			
+			// 현재 노드의 자식노드중 더 작은 자식노드를 찾는다.
 			if ((child < h->heap_size) &&
 				(strcmp(h->heap[child]->years, h->heap[child + 1]->years) > 0))
 				child++;
 			if (strcmp(temp->years, h->heap[child]->years) <= 0) break;
-			
+			// 한단계 아래로 이동
 			h->heap[parent] = h->heap[child];
 			parent = child;
 			child *= 2;
@@ -251,36 +257,37 @@ element delete_max_heap(HeapType *h, char *type)
 	return item;
 }
 
-
+// 현재 요소의 개수가 heap_size인 히프 h에 item을 삽입한다.
+// 삽입 함수
 void insert_max_heap(HeapType *h, element item, char *type)
 {
 	int i;
 	i = ++(h->heap_size);
 	if (strcmp(type, "title")==0) {
-		
+		//  트리를 거슬러 올라가면서 부모 노드와 비교하는 과정
 		while ((i != 1) && (strcmp(item->title, h->heap[i / 2]->title) < 0)) {
 			h->heap[i] = h->heap[i / 2];
 			i /= 2;
 		}
 	}
 	else if(strcmp(type,"years")==0){
-		
+		//  트리를 거슬러 올라가면서 부모 노드와 비교하는 과정
 		while ((i != 1) && (strcmp(item->years, h->heap[i / 2]->years) < 0)) {
 			h->heap[i] = h->heap[i / 2];
 			i/=2;
 		}
 	}
-	h->heap[i] = item;
+	h->heap[i] = item;     // 새로운 노드를 삽입
 	//printf("%-10s %-70s %-10s %-10s\n",h->heap[i]->no, h->heap[i]->title, h->heap[i]->years, h->heap[i]->genre);
 }
 
-
+// 초기화 함수
 void init(HeapType *h)
 {
 	h->heap_size = 0;
 }
 
-
+//기수 정렬
 int getMax(movieData list[], int n)
 {
 	int max = atoi(list[0].years);
